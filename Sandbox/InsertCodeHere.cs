205cd7e54c727f2f8cdf7d3faadfb35b2f63187b
Dshy <KortSkg@KORTSKG-PC>
2016-10-11 13:51:02 +0200

    Den viser nu et nummer

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,10 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            MysticNumbers MN = new MysticNumbers();
+            MN.ThreeNumbers(2, 3, 4);
+            Console.WriteLine($"Numrene er {MN.ThreeNumbers(2,3,4)}");
+
             // The LAST line of code should be ABOVE this line
         }
     }
